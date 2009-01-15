defc0a067dc8cd86f0c3074b42836e3b752945f1
Will Boyce <mail@willboyce.com>
2009-01-15 22:20:22 +0000

    Added language detection

--- README.md
+++ README.md
@@ -5,8 +5,10 @@ A Library for using Google Translate in Python
 
 ## Usage
 
-        >>> import gtrans
-        >>> gtrans.translate(src="english", to="spanish", phrase="Hello World")
-        u'Hola Mundo'
-        >>> gtrans.translate(src="spanish", to="english", phrase="Hola Mundo")
-        u'Hello World'
+		>>> import gtrans
+		>>> gtrans.translate("Hello World!", to="es") # 'src' not specified, language detected
+		'Hola Mundo!'
+		>>> gtrans.translate("Hola Mundo!", to="french")
+		'Bonjour tout le monde!'
+		>>> gtrans.translate("Bonjour tout le monde!", to="english", src="fr")
+		'Hello everyone!'
--- gtrans.py
+++ gtrans.py
@@ -8,24 +8,39 @@ class UrlOpener(urllib.FancyURLopener):
 	version = "py-gtranslate/1.0"
 
 class InvalidLanguage(Exception): pass
+class UnableToDetectLanguage(Exception): pass
 
 base_uri = "http://ajax.googleapis.com/ajax/services/language/translate"
+detect_uri = "http://ajax.googleapis.com/ajax/services/language/detect"
 default_params = {'v': '1.0'}
 langs = json.load(file('langs.json', 'r'))
 
-def translate(src, to, phrase):
+def _build_args(dict):
+	args = default_params.copy()
+	args.update(dict)
+	return '&'.join(['%s=%s' % (k,v) for (k,v) in args.iteritems()])
+
+def detect_lang(phrase):
+	args = {'q': urllib.quote_plus(phrase)}
+	resp = json.load(UrlOpener().open('%s?%s' % (detect_uri, _build_args(args))))
+	try:
+		return resp['responseData']['language']
+	except:
+		raise UnableToDetectLanguage()
+
+def translate(phrase, to, src="auto"):
+	if src == "auto":
+		src = detect_lang(phrase)
 	src = langs.get(src, src)
 	to = langs.get(to, to)
 	if not src in langs.values() or not to in langs.values():
 		raise InvalidLanguage("%s=>%s is not a valid translation" % (src, to))
 	
-	args = default_params.copy()
-	args.update({
+	args = {
 		'langpair': '%s%%7C%s' % (src, to),
 		'q': urllib.quote_plus(phrase),
-	})
-	argstring = '%s' % ('&'.join(['%s=%s' % (k,v) for (k,v) in args.iteritems()]))
-	resp = json.load(UrlOpener().open('%s?%s' % (base_uri, argstring)))
+	}
+	resp = json.load(UrlOpener().open('%s?%s' % (base_uri, _build_args(args))))
 	try:
 		return resp['responseData']['translatedText']
 	except:
